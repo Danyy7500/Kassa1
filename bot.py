e66ac809bb81a9035162f225d9f8f7e9f4f3fc5c
Danyy7500 <daniltishchenko725@gmail.com>
2025-12-01 22:44:22 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@
 
 TOKEN = "8595625321:AAHKYX0k8pJH28f2H3VJuN9Q8MMKOmIIITw" 
 
-import telebot
 import json
 from datetime import datetime
 from openpyxl import Workbook
@@ -164,6 +163,7 @@ def excel(message):
 
 # ---------- ЗАПУСК ----------
 bot.polling(none_stop=True)
+
 
 
 
