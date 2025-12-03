d1c75ff3aaed6c08c5b4e85257c0bf086acfc32c
Danyy7500 <daniltishchenko725@gmail.com>
2025-12-03 20:06:06 +0200

    Refactor bot.py for improved structure and clarity

--- bot.py
+++ bot.py
@@ -5,9 +5,6 @@
 TOKEN = "8595625321:AAHKYX0k8pJH28f2H3VJuN9Q8MMKOmIIITw" 
 
 
-import json
-from datetime import datetime
-from openpyxl import Workbook
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 import telebot
@@ -165,6 +162,7 @@ def excel(message):
 
 # ---------- ЗАПУСК ----------
 bot.polling(none_stop=True)
+
 
 
 
